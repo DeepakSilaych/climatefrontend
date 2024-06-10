109ce4fe3aa54975fdcef704360a6dcd78e431c2
gulshan0007 <121476861+gulshan0007@users.noreply.github.com>
2024-06-10 21:39:11 +0530

    legends

--- src/components/home/Legends.jsx
+++ src/components/home/Legends.jsx
@@ -32,10 +32,10 @@ const RainfallLegend = () => {
         <div style={legendStyle} className='z-30 font-merriweather'>
             <h4>Rainfall (per day)</h4>
             <div style={itemStyle}><span style={colorBoxStyle('green')}></span> No Rain</div>
-            <div style={itemStyle}><span style={colorBoxStyle('#acff26')}></span> 0.1 - 7.5 mm</div>
-            <div style={itemStyle}><span style={colorBoxStyle('yellow')}></span> 7.6 - 35.5 mm</div>
-            <div style={itemStyle}><span style={colorBoxStyle('orange')}></span> 35.6 - 124.4 mm</div>
-            <div style={itemStyle}><span style={colorBoxStyle('red')}></span> 124.5 mm & above</div>
+            <div style={itemStyle}><span style={colorBoxStyle('#acff26')}></span>Light Rain:- 0.1 - 7.5 mm</div>
+            <div style={itemStyle}><span style={colorBoxStyle('yellow')}></span>Moderate Rain:- 7.6 - 35.5 mm</div>
+            <div style={itemStyle}><span style={colorBoxStyle('orange')}></span>Heavy Rain:- 35.6 - 124.4 mm</div>
+            <div style={itemStyle}><span style={colorBoxStyle('red')}></span>Extreme Rain:- 124.5 mm & above</div>
         </div>
     );
 };
