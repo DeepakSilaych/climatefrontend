67108b166bbbfaa879ccc2547eb9c45d6ba9b799
DeepakSilaych <deepaksilaych@gmail.com>
2024-06-17 02:21:07 +0530

    about

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import ReactGA from "react-ga";
 import "./styles.css";
 
 import App from "./App";
+const TRACKING_ID = "G-76SVHYWKZX";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
+ReactGA.initialize(TRACKING_ID);
 
 root.render(
   <StrictMode>
